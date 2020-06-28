05157ad2eefcfe34889ef6de98b510f778dc03f4
liqkm <liqkm@yonyou.com>
2020-06-28 22:54:25 +0800

    请求第一步完成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildUrl } from './helpers/url'
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): void {
   // TODO
+  processParams(config)
   xhr(config)
+}
+
+function processParams(config: AxiosRequestConfig): void {
+  config.url = transforUrl(config)
+}
+
+function transforUrl(config: AxiosRequestConfig): string {
+  let { url, params } = config
+  return buildUrl(url, params)
 }
 
 export default axios
