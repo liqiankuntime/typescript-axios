b5423d552d5edaa5e763942e97ac88ebcdeff184
liqiankun <15896858071@163.com>
2020-10-12 18:05:14 +0800

    ts学习简要笔记

---
+++ src/learn.ts
@@ -0,0 +1,55 @@
+// https://ts.xcatliu.com/
+// 这里是学习 ts的简要笔记
+
+interface ApiError extends Error {
+  code: number
+}
+interface HttpError extends Error {
+  statusCode: number
+}
+
+function isApiError(error: Error) {
+  if (typeof (error as ApiError).code === 'number') {
+    return true
+  }
+  return false
+}
+
+function copyFields<T extends U, U>(target: T, source: U): T {
+  for (let id in source) {
+    // target[id] = (<T>source)[id];
+    // // target[id] = source[id];
+    target[id] = (source as T)[id]
+    // 将一个父类断言为一个更加具体的子类
+    // (泛型的类型 一个是T类型，一个是U类型；这里的T继承了U;
+    // 所以T是子类型； 断言的语法： 值 as 类型)
+  }
+  return target
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 }
+
+copyFields(x, { b: 10, d: 20 })
+
+interface Cat {
+  name: string
+  run(): void
+}
+interface Fish {
+  name: string
+  swim(): void
+}
+const tom: Cat = {
+  name: 'Tom',
+  run() {
+    console.log('run')
+  }
+}
+function isFish(animal: Cat | Fish) {
+  if (typeof (animal as Fish).swim === 'function') {
+    return true
+  }
+  return false
+}
+
+isFish(tom)
